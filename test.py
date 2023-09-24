ac5360a8a97b26b9222af67e46e9e58382044f18
u032582 <u032582@gmail.com>
2023-09-24 09:14:28 +0900

    init

---
+++ test.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from pandas import DataFrame, Series
+import matplotlib.pyplot as plt
+
+
+from datetime import datetime
+import pytz
+
+def parse_str(x):
+    """
+    Returns the string delimited by two characters.
+
+    Example:
+        `>>> parse_str('[my string]')`
+        `'my string'`
+    """
+    return x[1:-1]
+
+def parse_datetime(x):
+    '''
+    Parses datetime with timezone formatted as:
+        `[day/month/year:hour:minute:second zone]`
+
+    Example:
+        `>>> parse_datetime('13/Nov/2015:11:45:42 +0000')`
+        `datetime.datetime(2015, 11, 3, 11, 45, 4, tzinfo=<UTC>)`
+
+    Due to problems parsing the timezone (`%z`) with `datetime.strptime`, the
+    timezone will be obtained using the `pytz` library.
+    '''
+    try:
+        dt = datetime.strptime(x[1:-7], '%d/%b/%Y:%H:%M:%S')
+        dt_tz = int(x[-6:-3])*60+int(x[-3:-1])
+        return dt.replace(tzinfo=pytz.FixedOffset(dt_tz))
+    except Exception as e:
+        return e
+
+import re
+import pandas as pd
+
+data = pd.read_csv(
+    'access_log',
+    sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+    engine='python',
+    na_values='-',
+    header=None,
+    usecols=[0, 3, 4, 5, 6, 7, 8],
+    names=['ip', 'time', 'request', 'status', 'size', 'referer', 'user_agent'],
+    converters={'time': parse_datetime,
+                'request': str,
+                'status': str,
+                'size': str,
+                'referer': str,
+                'user_agent': str})
+
+pd.set_option('display.width', 10000)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+print(data)
+
+
+
+
